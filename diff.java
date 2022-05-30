0367124a38397fb9b2df63e0af1c880fd74134ee
Anjali Singh <anjalisingh1373@gmail.com>
2022-05-30 10:02:31 +0530

    subtraction in diff.java

--- diff.java
+++ diff.java
@@ -1,3 +1,9 @@
 public class diff{
-    
+    public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, multi;
+      multi = num1 - num2;
+
+      System.out.println("Subtraction of numbers: "+multi);
+   }
 }
