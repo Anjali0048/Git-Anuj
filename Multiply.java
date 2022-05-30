e91505e6221343750e78109243dcc5a12be3729a
Anjali Singh <anjalisingh1373@gmail.com>
2022-05-30 08:05:21 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply{
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, multi;
+      multi = num1 * num2;
+
+      System.out.println("Multiplication of numbers: "+multi);
+   }
+}
